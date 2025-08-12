966b75d0c152248d337f4b65f915c04231dca489
fardin-annonlab <fardin@annonlab.com>
2025-08-13 01:03:04 +0600

    complete 5.2

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,6 +1,7 @@
 import { Template } from 'meteor/templating';
 import './App.html';
 import { TasksCollection } from "../api/TasksCollection";
+import './Task.js';
 
 
 Template.mainContainer.helpers({
