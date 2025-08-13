a826f6225b8cb41cf121dd7a81416616edf2588e
fardin-annonlab <fardin@annonlab.com>
2025-08-14 02:39:27 +0600

    complete 7.8

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,33 +2,94 @@ import { Template } from 'meteor/templating';
 import './App.html';
 import { TasksCollection } from "../api/TasksCollection";
 import './Task.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import "./Login.js";
+
+const HIDE_COMPLETED_STRING = 'hideCompleted';
+const getUser = () => Meteor.user();
+const isUserLogged = () => !!Meteor.userId();
 
 
-Template.mainContainer.helpers({
-    tasks() {
-        return TasksCollection.find({}, { sort: { createdAt: -1 } });
-    },
-    // tasks: [
-    //     { text: 'This is task 1' },
-    //     { text: 'This is task 2' },
-    //     { text: 'This is task 3' },      
-    // ],
-    name(){
-        return 'I Am Shahid Afridi'
-    }
+const getTasksFilter = () => {
+  const user = getUser();
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const userFilter = user ? { userId: user._id } : {};
+
+  const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
+
+  return { userFilter, pendingOnlyFilter };
+}
+
+
+Template.mainContainer.onCreated(function mainContainerOnCreated() {
+  this.state = new ReactiveDict();
+  this.state.set(HIDE_COMPLETED_STRING, false); 
 });
 
-Template.form.events({
-    'submit .task-form'(events){
-        events.preventDefault();
-        const target = events.target;
-        const task = target.text.value;
-        console.log(task);
-        TasksCollection.insert({
-            text: task,
-            createdAt: new Date()
-        });
+Template.mainContainer.helpers({
+   tasks() {
+    const instance = Template.instance();
+    const hideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
 
-        target.text.value = '';
+    const { pendingOnlyFilter, userFilter } = getTasksFilter();
+
+    if (!isUserLogged()) {
+      return [];
     }
-})
+
+    return TasksCollection.find(hideCompleted ? pendingOnlyFilter : userFilter, {
+      sort: { createdAt: -1 },
+    }).fetch();
+  },
+  hideCompleted() {
+    return Template.instance().state.get(HIDE_COMPLETED_STRING);
+  },
+  name() {
+    return 'I Am Shahid Afridi';
+  },
+  incompleteCount() {
+    if (!isUserLogged()) {
+      return '';
+    }
+
+    const { pendingOnlyFilter } = getTasksFilter();
+
+    const incompleteTasksCount = TasksCollection.find(pendingOnlyFilter).count();
+    return incompleteTasksCount ? `(${incompleteTasksCount})` : '';
+  },
+  isUserLogged() {
+    return isUserLogged();
+  },
+  getUser() {
+    return getUser();
+  }
+});
+
+
+Template.mainContainer.events({
+  "click #hide-completed-button"(event, instance) {
+    const current = instance.state.get(HIDE_COMPLETED_STRING);
+    instance.state.set(HIDE_COMPLETED_STRING, !current);
+  },
+  'click .user'() {
+    Meteor.logout();
+  }
+});
+
+
+Template.form.events({
+  'submit .task-form'(events){
+    events.preventDefault();
+    const target = events.target;
+    const task = target.text.value?.trim();
+    if (!task) return;
+    TasksCollection.insert({
+      text: task,
+      createdAt: new Date(),
+      isChecked: false, 
+    });
+    target.text.value = '';
+  },
+});
--- server/main.js
+++ server/main.js
@@ -1,11 +1,35 @@
 import { Meteor } from 'meteor/meteor';
 import { TasksCollection } from '/imports/api/TasksCollection';
+import { Accounts } from 'meteor/accounts-base';
 
-const insertTask = async taskText => {
-  await TasksCollection.insertAsync({ text: taskText });
-};
+
+const SEED_USERNAME = 'fardin';
+const SEED_PASSWORD = '123456789';
+
+// const insertTask = async taskText => {
+//   await TasksCollection.insertAsync({ text: taskText });
+// };
+
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
 
 Meteor.startup(async () => {
+  
+  let user = Accounts.findUserByUsername(SEED_USERNAME);
+
+  if (!user) {
+    console.log(`Creating default user "${SEED_USERNAME}"...`);
+    const userId = Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+    user = Meteor.users.findOne(userId);
+  }
+
   const count = await TasksCollection.find().countAsync();
   if (count === 0) {
     const tasks = [
